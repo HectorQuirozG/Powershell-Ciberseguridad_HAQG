d9f08d579dece773f281c183fe2d924c7fbeb37e
HectorQuirozG <hector.quirozg@uanl.edu.mx>
2025-08-20 20:30:52 -0600

    Add files via upload

---
+++ E6/auditoriabasica.psd1
@@ -0,0 +1,6 @@
+﻿New-ModuleManifest -path '.\auditoriabasica.psd1' `
+    -rootmodule 'auditoriabasica.psm1' `
+    -moduleversion '1.0.0' `
+    -author 'Equipo de Ciberseguridad' `
+    -description 'Módulo para auditoría básica de usuarios y servicios' `
+    -powershellversion '5.1'
---
+++ E6/main.ps1
@@ -0,0 +1,26 @@
+﻿Clear-Host
+Import-Module auditoriabasica
+
+Write-Host "=== Auditoría básica de usuarios y servicios ==="
+Write-Host "1. Mostrar usuarios inactivos"
+Write-Host "2. Mostrar servicios externos activos"
+
+$op = Read-Host "Selecciona una opción: "
+
+switch ($op) {
+    "1" {
+        $usuarios = obtener-usuariosinactivos
+        $usuarios | Format-Table name, enabled, lastlogon -AutoSize
+        $usuarios | Export-Csv -Path ".\users_inac.csv" -NoTypeInformation
+        Write-Host "`n📄 Reporte generado: users_inac.csv"
+    }
+    "2" {
+        $servicios = obtener-serviciosexternos
+        $servicios | Format-Table displayname, status, starttype -AutoSize
+        $servicios | ConvertTo-Html | Out-File ".\serv_e.html"
+        Write-Host "`n📄 Reporte generado: serv_e.html"
+    }
+    default {
+        Write-Host "Opción inváida"
+    }
+}
